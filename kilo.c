97952125c52c619bd2297a3b7ff6874e3f21dfe9
reyes-byte <153059433+reyes-byte@users.noreply.github.com>
2026-01-31 13:48:01 +0800

    Subject:Get the size of the terminal
    Technical documentation
    ioctl() place the number of columns wide and
    the number of rows high the terminal is into
    the given winsize struct
    TIOCGWIINSZ TErminal IOCTL which stands for
    Input/Output Control Get Window SIze

--- kilo.c
+++ kilo.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -9,7 +10,12 @@
 //
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-struct termios orig_termios;
+// store the width and height of the terminal
+struct editorConfig {
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 // terminal
 void die(const char *s) {
@@ -22,16 +28,16 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios);
 }
 
 void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
         die("tcgetattr");
     };
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK |ISTRIP| IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -53,10 +59,35 @@ char editorReadKey() {
     return c;
 }
 
+//get window size
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1|| ws.ws_col == 0) {
+        return -1
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /*** output  ***/
+void editorDrawRows() {
+    int y;
+    for (y = 0; y < 24; ++y) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void editorRefreshScreen() {
     write(STDOUT_FILENO, "\x1b[2J", 4); 
     write(STDOUT_FILENO, "\x1b[H]", 3); //default is row 1 column 1
+
+    editorDrawRows();
+
+    write(STDOUT_FILENO, "\x1b[H]", 3); //default is row 1 column 1
+    
 }
 
 /***input ***/
