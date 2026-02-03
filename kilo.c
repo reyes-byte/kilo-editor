4613f93ec924dc44aa83b7f1a0deef08cda602a6
reyes-byte <153059433+reyes-byte@users.noreply.github.com>
2026-02-03 22:59:41 +0800

    sub:Implemened appending buffer
    Issue: it's not a good idea to make a whole bunch
    of small write()'s everytime we refresh the screen.
    It would be better to do one biig write(), to make
    sure the whole screen updates at once/
    
    Solution: replace all our write() calls with code
    that appends the string to  abuffer, and then write()
    this buffer out at the end.
    
    Technical implementation:
    create a buffer using a struct
    and then create a temp
    variable called new(prevent corrupting
    original data) which use realloc which reallocates
    additional memory to the original memory block
    of the buffer
    realloc(): give it a ptr of current memory
    and the new size you want which is len(original) +
    new length in this case
    and then memcpy the original string memory
    with the number of bytes into the buffer
    &new[ab->len] is the empty slot
    s is the string data
    len is the length of string s
    copy string data to after &new[ab->len]

--- kilo.c
+++ kilo.c
@@ -5,6 +5,10 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
+#include <string.h>
+
+
+
 
 // defines
 //
@@ -83,7 +87,7 @@ int getCursorPosition(int *rows, int *cols) {
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
-    if (1||ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1|| ws.ws_col == 0) {
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1|| ws.ws_col == 0) {
         if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
         return getCursorPosition(rows, cols);
     } else {
@@ -93,11 +97,37 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+/*** append buffer ***/
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL,0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab ->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab -> b);
+}
+
 /*** output  ***/
 void editorDrawRows() {
     int y;
     for (y = 0; y < E.screenrows; ++y) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        write(STDOUT_FILENO, "~", 1);
+    
+        if (y < E.screenrows - 1) {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
+    
     }
 }
 
