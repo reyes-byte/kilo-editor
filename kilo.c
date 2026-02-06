b96356351f7be8169d4cba94e5253fa195d02223
reyes-byte <153059433+reyes-byte@users.noreply.github.com>
2026-02-06 23:18:52 +0800

    sub: Added HOME and END keys in the TE
    move to start of col and end of screencols

--- kilo.c
+++ kilo.c
@@ -20,6 +20,8 @@ enum editorKey {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN
 };
@@ -85,8 +87,12 @@ int editorReadKey() {
                 if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
                 if (seq[2] == '~'){
                     switch(seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '4': return END_KEY;
                         case '5': return PAGE_DOWN;
                         case '6': return PAGE_UP;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
                     }
                 }
             } else {
@@ -95,13 +101,23 @@ int editorReadKey() {
                     case 'B': return ARROW_DOWN;
                     case 'C': return ARROW_RIGHT;
                     case 'D': return ARROW_LEFT;
+                    case 'F': return END_KEY;
+                    case 'H': return HOME_KEY;
                 }
             }
         }
+        else if (seq[0] == '0') {
+            switch(seq[1]) {
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
+            }
+        }
+
         return '\x1b';
     } else {
         return c;
     }
+    
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -238,6 +254,12 @@ void editorProcessKeypress()
             write(STDOUT_FILENO, "\x1b[2J", 4); 
             write(STDOUT_FILENO, "\x1b[H]", 3);
             exit(0);
+            break;
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols - 1;
             break;
         case PAGE_UP:
         case PAGE_DOWN:
