8f94fc6358d28f2eebcef969b375a7e6f271896b
reyes-byte <153059433+reyes-byte@users.noreply.github.com>
2026-02-08 15:16:34 +0800

    sub: implemented basic version of editoropen()
    editoropen() will eventually be for opening and
    reading files

--- kilo.c
+++ kilo.c
@@ -20,17 +20,26 @@ enum editorKey {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    DEL_KEY,
     HOME_KEY,
     END_KEY,
     PAGE_UP,
     PAGE_DOWN
 };
 
+typedef struct erow {
+    int size;
+    char *chars;
+} erow;
+
+
 // store the width and height of the terminal
 struct editorConfig {
     int cx, cy;
     int screenrows;
     int screencols;
+    int numrows;
+    erow row;
     struct termios orig_termios;
     
 };
@@ -88,6 +97,7 @@ int editorReadKey() {
                 if (seq[2] == '~'){
                     switch(seq[1]) {
                         case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
                         case '4': return END_KEY;
                         case '5': return PAGE_DOWN;
                         case '6': return PAGE_UP;
@@ -149,6 +159,17 @@ int getWindowSize(int *rows, int *cols) {
         *rows = ws.ws_row;
         return 0;
     }
+}
+
+/*    file i/o     */
+void editorOpen() {
+    char *line = "Hello world!";
+    ssize_t linelen = 13;
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
 }
 
 /*** append buffer ***/
@@ -283,12 +304,13 @@ void initEditor() {
 
     E.cx = 0;
     E.cy = 0;
-
+    E.numrows = 0;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 int main() {
     enableRawMode();
     initEditor();
+    editorOpen(); //opening and reading file from disk
     //raw mode entrered
 
     while(1) {
