5a588025d8c7d73bb7d5dc15e6821826cca2b013
reyes-byte <153059433+reyes-byte@users.noreply.github.com>
2026-02-26 15:21:38 +0800

    sub: Limit scrolling to the right of the screen
    does not allow user to scroll past the end of the Lines:
    implementation details:
    if (row && E.cx < row->size)
    
    does not user to scroll down the last line:
    implemntation details:
    erow *row = (E.cy >= E.numrows) NULL : &E.row[E.cy]

--- kilo.c
+++ kilo.c
@@ -301,6 +301,7 @@ void editorRefreshScreen() {
 
 /***input ***/
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
     switch(key) {
         case ARROW_LEFT:
             if (E.cx != 0){
@@ -308,6 +309,7 @@ void editorMoveCursor(int key) {
             }
             break;
         case ARROW_RIGHT:
+            if (row && E.cx < row ->size)
                 E.cx++;
             break;
         case ARROW_UP: 
