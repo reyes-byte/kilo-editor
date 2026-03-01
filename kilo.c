4bb54b2f7fdd01646fc9bcccffc10b5d7a98db17
reyes-byte <153059433+reyes-byte@users.noreply.github.com>
2026-03-01 11:48:23 +0800

    Notes: Backspace is ascii 8 and asccii 127 is delete in modern computing backspace is 127 and \x1b[3~ is DEL. Lastly, we handle Ctrl-L and Escape by not doing anything when those keys are pressed CTRL-L is traditionally used to refresh the screen in terminal programs. But our text edit, screen refreshes after any keypress.

--- kilo.c
+++ kilo.c
@@ -25,6 +25,7 @@
 
 
 enum editorKey {
+    BACKSPACE = 127,
     ARROW_LEFT = 1000, 
     ARROW_RIGHT,
     ARROW_UP,
@@ -467,6 +468,9 @@ void editorProcessKeypress()
     int c = editorReadKey();
 
     switch(c) {
+        case '\r':
+            /*TODO*/
+            break;
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J", 4); 
             write(STDOUT_FILENO, "\x1b[H", 3);
@@ -480,6 +484,14 @@ void editorProcessKeypress()
                 E.cx = E.row[E.cy].size;
             }
             break;
+        
+        case BACKSPACE:
+        case CTRL_KEY('h'):
+        case DEL_KEY:
+            /* TODO */
+            break;
+            
+        
         case PAGE_UP:
         case PAGE_DOWN:
             {
@@ -497,7 +509,9 @@ void editorProcessKeypress()
         case ARROW_LEFT:
             editorMoveCursor(c);
             break;   
-
+        case CTRL_KEY('l'):
+        case '\x1b':
+            break;
         default:
             editorInsertChar(c);
             break;
